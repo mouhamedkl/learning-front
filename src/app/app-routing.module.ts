293440bb630f2bd86f1005a390ff03d8f01e5746
mouhamedkl <mohamedamine.klai@esprit.tn>
2023-07-05 20:11:18 +0100

    add projet

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,11 +5,13 @@ import { ContactComponent } from './contact/contact.component';
 import { BlogComponent } from './blog/blog.component';
 import { CoursesComponent } from './courses/courses.component';
 import { AboutComponent } from './about/about.component';
+import { CoursesDetailsComponent } from './courses-details/courses-details.component';
 
 const routes: Routes = [
   { path:'', redirectTo:"index",pathMatch:"full"},
   {path:'index',component:IndexComponent},
   {path:'contact',component:ContactComponent},
+  {path:'details',component:CoursesDetailsComponent},
 
   {path:'blog',component:BlogComponent},
 
