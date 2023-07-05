514babdc5e4b533c78b9f0d0952aeccfc2e5b6c5
mouhamedkl <mohamedamine.klai@esprit.tn>
2023-07-05 17:15:42 +0100

    add projet

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { IndexComponent } from './index/index.component';
+import { ContactComponent } from './contact/contact.component';
+import { BlogComponent } from './blog/blog.component';
+import { CoursesComponent } from './courses/courses.component';
+import { AboutComponent } from './about/about.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'', redirectTo:"index",pathMatch:"full"},
+  {path:'index',component:IndexComponent},
+  {path:'contact',component:ContactComponent},
+
+  {path:'blog',component:BlogComponent},
+
+  {path:'course',component:CoursesComponent},
+
+  {path:'about',component:AboutComponent},
+
+
+  //{ path: '**', component: PageNotFoundComponent },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,37 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IndexComponent } from './index/index.component';
+import { AboutComponent } from './about/about.component';
+import { CoursesComponent } from './courses/courses.component';
+import { BlogComponent } from './blog/blog.component';
+import { CoursesDetailsComponent } from './courses-details/courses-details.component';
+import { ContactComponent } from './contact/contact.component';
+import { FormsModule } from '@angular/forms';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent,
+    AboutComponent,
+    CoursesComponent,
+    BlogComponent,
+    CoursesDetailsComponent,
+    ContactComponent,
+    NavbarComponent,
+
+
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+  BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
